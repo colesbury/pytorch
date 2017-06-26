c6947ee9640609d2744cc2ec85ed0b34ff755fd5
Sam Gross <sgross@fb.com>
2017-06-26 08:13:35 -0700

    Use torch.matmul in nn.Linear
    This takes advantage of the broadcasting behavior of torch.matmul to
    support inputs with more than two dimensions. The extra dimensions are
    treated like part of the batch dimension, much like nn.Bottle in Lua
    Torch.

--- torch/nn/functional.py
+++ torch/nn/functional.py
@@ -531,10 +531,16 @@ def sigmoid(input):
 # etc.
 
 def linear(input, weight, bias=None):
-    if bias is None:
-        return _functions.linear.Linear.apply(input, weight)
-    else:
-        return _functions.linear.Linear.apply(input, weight, bias)
+    size = input.size()
+    if input.dim() == 3:
+        input = input.view(size[0] * size[1], size[2])
+    output = input.mm(weight.t())
+    if len(size) == 3:
+        output = output.view(size[0], size[1], -1)
+    # output = input.matmul(weight.t())
+    if bias is not None:
+        output += bias.expand_as(output)
+    return output
 
 
 def bilinear(input1, input2, weight, bias=None):
