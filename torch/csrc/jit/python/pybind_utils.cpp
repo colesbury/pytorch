5af92b377db0b12b03fb27519fb39782996713d5
Sam Gross <colesbury@gmail.com>
2022-06-27 15:52:24 -0700

    Fix access to registered_instances

--- torch/csrc/jit/python/pybind_utils.cpp
+++ torch/csrc/jit/python/pybind_utils.cpp
@@ -9,14 +9,14 @@ namespace jit {
 // C++->Python. We need this because otherwise we may get the old Python object
 // if C++ creates a new object at the memory location of the deleted object.
 void clear_registered_instances(void* ptr) {
-  auto& registered_instances =
-      pybind11::detail::get_internals().registered_instances;
-  auto range = registered_instances.equal_range(ptr);
-  for (auto it = range.first; it != range.second; ++it) {
-    auto vh = it->second->get_value_and_holder();
-    vh.set_instance_registered(false);
-  }
-  registered_instances.erase(ptr);
+  pybind11::detail::with_instance_map(ptr, [&](pybind11::detail::instance_map &registered_instances) {
+    auto range = registered_instances.equal_range(ptr);
+    for (auto it = range.first; it != range.second; ++it) {
+      auto vh = it->second->get_value_and_holder();
+      vh.set_instance_registered(false);
+    }
+    registered_instances.erase(ptr);
+  });
 }
 
 IValue toIValue(py::handle obj, const TypePtr& type, c10::optional<int32_t> N) {
